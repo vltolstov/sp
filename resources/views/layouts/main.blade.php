7656f159672d96a312451de49fe06e050ad76a53
vltolstov <tolstov@gmail.com>
2023-01-11 11:07:43 +0700

    Добавлен Vite и базовый шаблон

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+
+    <base href="{{$baseUrl}}">
+    <title>Тест</title>
+
+    @vite('resources/css/app.css')
+
+    <link rel="canonical" href="{{$baseUrl}}"/>
+
+</head>
+<body>
+
+<p>Тест</p>
+
+@vite('resources/js/app.js')
+
+</body>
+</html>
