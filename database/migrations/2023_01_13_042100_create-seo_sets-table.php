f47b46dac3fc68c98de9ed8ada80432f566ca9bf
vltolstov <tolstov@gmail.com>
2023-01-13 11:29:26 +0700

    Добавлен MC наборов сео параметров для страниц

---
+++ database/migrations/2023_01_13_042100_create-seo_sets-table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('seo_sets', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 70)->nullable();
+            $table->string('description', 160)->nullable();
+            $table->string('keywords', 255)->nullable();
+            $table->decimal('priority', $precision = 1, $scale = 1)->default(0.5);
+            $table->string('changefreq')->default('weekly');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
