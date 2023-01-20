77d4fd3ee4ff86fb0b122caee9a9a64d2e407639
vltolstov <tolstov@gmail.com>
2023-01-20 15:24:54 +0700

    Сверстана шапка под col-lg

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,52 @@
 @extends('layouts.main')
+
+@section('test-layout')
+
+<div class="head">
+    <div class="container">
+        <header>
+            <div class="head-flex">
+                <div class="logo">
+                    <img src="/images/logo.png" height="64" alt="logo">
+                </div>
+                <div class="head-menu">
+                    <ul>
+                        <li><a href="#">Раздел №1</a></li>
+                        <li><a href="#">Раздел №2</a></li>
+                        <li><a href="#">Раздел №3</a></li>
+                        <li><a href="#">Раздел №4</a></li>
+                        <li><a href="#">Рвздел №5</a></li>
+                    </ul>
+                </div>
+                <div class="head-contacts">
+                    <div class="contacts-block">
+                        <p><a class="main-tel" href="tel:80000000000">8 000 000-00-00</a></p>
+                        <p><a class="mobile-tel" href="tel:70000000000">+7 000 000-00-00</a></p>
+                        <p><a class="head-mail" href="mailto:email@mail.ma">email@mail.ma</a></p>
+                        <p class="head-city">г. Новосибирск</p>
+                    </div>
+                </div>
+            </div>
+        </header>
+    </div>
+</div>
+<div class="slider">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                слайд
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="offers">
+    <div class="container">
+        <div class="row">
+            офферы
+        </div>
+    </div>
+</div>
+
+
+@endsection
