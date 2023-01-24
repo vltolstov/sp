e68eb54850ac170b27f1001aadb89f0507ba5618
vltolstov <tolstov@gmail.com>
2023-01-24 13:45:58 +0700

    Добавлена верстка блока каталога на главной странице

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -101,8 +101,8 @@
     <div class="container">
         <div class="row special-offer-wrap">
             <div class="col-lg-12">
-                <div class="block-headers">
-                    <h3>Заголовок третьего уровня</h3>
+                <div class="special-offer-headers">
+                    <h3>Специальное предложение</h3>
                     <p>Подзаголовок - однострочное описание</p>
                 </div>
                 <div class="row special-offer-flex">
@@ -131,9 +131,104 @@
 
 <div class="catalog">
     <div class="container">
-        <div class="row">
-            <div class="col-lg-12">
-                каталог
+        <div class="row catalog-wrap">
+            <div class="col-lg-5">
+                <div class="catalog-headers">
+                    <h3>Каталог</h3>
+                    <p>Подзаголовок</p>
+                </div>
+                <p class="catalog-info">Повседневная практика показывает, что консультация с широким активом способствует подготовки
+                    и реализации дальнейших направлений развития.</p>
+                <p class="catalog-info">Идейные соображения высшего порядка, а также
+                    постоянное информационно-пропагандистское обеспечение нашей деятельности играет важную роль
+                    в формировании существенных финансовых и административных условий.</p>
+                <div class="catalog-button">
+                    <a href="#">Показать еще</a>
+                </div>
+            </div>
+            <div class="col-lg-7">
+                <div class="row">
+                    <div class="col-lg-3">
+                        <div class="catalog-item">
+                            <a href="#">
+                                <div class="catalog-icon">
+                                    <img src="/images/test-1000x750.png">
+                                </div>
+                                <p>Название</p>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="catalog-item">
+                            <a href="#">
+                                <div class="catalog-icon">
+                                    <img src="/images/test-1000x750.png">
+                                </div>
+                                <p>Название</p>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="catalog-item">
+                            <a href="#">
+                                <div class="catalog-icon">
+                                    <img src="/images/test-1000x750.png">
+                                </div>
+                                <p>Название</p>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="catalog-item">
+                            <a href="#">
+                                <div class="catalog-icon">
+                                    <img src="/images/test-1000x750.png">
+                                </div>
+                                <p>Название</p>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="catalog-item">
+                            <a href="#">
+                                <div class="catalog-icon">
+                                    <img src="/images/test-1000x750.png">
+                                </div>
+                                <p>Название</p>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="catalog-item">
+                            <a href="#">
+                                <div class="catalog-icon">
+                                    <img src="/images/test-1000x750.png">
+                                </div>
+                                <p>Название</p>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="catalog-item">
+                            <a href="#">
+                                <div class="catalog-icon">
+                                    <img src="/images/test-1000x750.png">
+                                </div>
+                                <p>Название</p>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="catalog-item">
+                            <a href="#">
+                                <div class="catalog-icon">
+                                    <img src="/images/test-1000x750.png">
+                                </div>
+                                <p>Название</p>
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
