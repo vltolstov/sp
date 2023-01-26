afbe62595aaf6e2ee2abd54f11ce9b6d8ce1423b
vltolstov <tolstov@gmail.com>
2023-01-26 15:55:25 +0700

    Добавлена верстка блока новостей на главной странице

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -294,10 +294,66 @@
 
 <div class="news">
     <div class="container">
-        <div class="row">
+        <div class="row news-headers-wrap">
             <div class="col-lg-12">
-                новости
+                <div class="catalog-headers">
+                    <h3>Новости</h3>
+                    <p>Самое важное</p>
+                </div>
             </div>
+        </div>
+        <div class="row news-wrap">
+            <div class="col-lg-12">
+                <div class="row flex">
+                    <div class="col-lg-4">
+                        <div class="news-block">
+                            <div class="news-header news-special">
+                                <a href="#">Заголовок новости на две строки для проверки переноса</a>
+                            </div>
+                            <div class="news-text">
+                                <p>Повседневная практика показывает, что консультация с широким активом способствует подготовки
+                                    и реализации дальнейших направлений развития. Повседневная практика показывает, что консультация с широким активом способствует подготовки
+                                    и реализации дальнейших направлений развития.</p>
+                            </div>
+                            <div class="news-date">
+                                <p>00.00.0000</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="news-block">
+                            <div class="news-header">
+                                <a href="#">Заголовок новости на две строки для проверки переноса</a>
+                            </div>
+                            <div class="news-text">
+                                <p>Повседневная практика показывает, что консультация с широким активом способствует подготовки
+                                    и реализации дальнейших направлений развития. Повседневная практика показывает, что консультация с широким активом способствует подготовки
+                                    и реализации дальнейших направлений развития.</p>
+                            </div>
+                            <div class="news-date">
+                                <p>00.00.0000</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="news-block">
+                            <div class="news-header">
+                                <a href="#">Заголовок новости на две строки для проверки переноса</a>
+                            </div>
+                            <div class="news-text">
+                                <p>Повседневная практика показывает, что консультация с широким активом способствует подготовки
+                                    и реализации дальнейших направлений развития.</p>
+                            </div>
+                            <div class="news-date">
+                                <p>00.00.0000</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="news-button">
+            <a href="#">Показать еще</a>
         </div>
     </div>
 </div>
