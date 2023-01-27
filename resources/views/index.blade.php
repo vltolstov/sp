8f7179a039a1443782bc58fe386c927f262063e7
vltolstov <tolstov@gmail.com>
2023-01-27 16:35:17 +0700

    Добавлена верстка подвала

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -405,12 +405,45 @@
 </div>
 
 <div class="footer">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12">
-                контакты - подвал
+    <div class="row footer-flex">
+        <div class="col-lg-6">
+            <div class="footer-left">
+                <h3>Контакты</h3>
+                <p class="footer-post-header">Новосибирск, Улица 00, 00</p>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <p class="footer-mini-header">Станция метро</p>
+                        <p class="footer-info">Красный проспект</p>
+                    </div>
+                    <div class="col-lg-6">
+                        <p class="footer-mini-header">Режим работы</p>
+                        <p class="footer-info">Пн-Пт: 09:00-18:00</p>
+                    </div>
+                    <div class="col-lg-6">
+                        <p class="footer-mini-header">E-Mail</p>
+                        <p class="footer-info">email@email.email</p>
+                    </div>
+                    <div class="col-lg-6">
+                        <p class="footer-mini-header">Телефон</p>
+                        <p class="footer-info">8-000-000-00-00</p>
+                    </div>
+                </div>
+                <div class="footer-button">
+                    <a href="#">Написать сообщение</a>
+                </div>
             </div>
         </div>
+        <div class="col-lg-6">
+            <div class="footer-map">
+
+            </div>
+        </div>
+    </div>
+    <div class="footer-line">
+        <p>Подробно расскажем о наших услугах, видах работ, и типовых проектах,
+            рассчитаем стоимость и подготовим индивидуальное предложение! Подробно расскажем о наших услугах, видах работ, и типовых проектах,
+            рассчитаем стоимость и подготовим индивидуальное предложение! Подробно расскажем о наших услугах, видах работ, и типовых проектах,
+            рассчитаем стоимость и подготовим индивидуальное предложение!</p>
     </div>
 </div>
 
